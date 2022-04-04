d9dd2e37d86d641f11542dd127a61e3c992f1af6
Brain Mentors Pvt Ltd <brainmentorspvtltd@gmail.com>
2022-04-04 16:51:11 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,25 @@
+public class First {
+
+    public static void main(String[] args) {
+
+        int salary[]=new int[]{2,3,1,3,8};
+       System.out.println(salary.toString());
+
+       System.out.println(salary[1]);
+
+       // System.out.println(salary[0]);
+        //System.out.println(salary[1]);
+
+     //   for(int i=0;i<salary.length;i++)
+       // System.out.println(i+" "+salary[i]);
+
+        //Enhance For loop (Traverse)
+       // for(double element:salary)
+       // System.out.println(element);
+
+       First obj=new First();
+       System.out.println(obj);
+
+    }
+
+}
